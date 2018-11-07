63febed64a595ddcb5f2a73a2ffc65386f4c5ecb
Christopher Fisher <chrisfisher419@gmail.com>
2018-11-07 18:29:52 -0500

    commit

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -15,18 +15,15 @@ namespace SportsStore.WebUI.Controllers
         {
             repository = repo;
         }
-
         public PartialViewResult Menu(string category = null)
         {
-
             ViewBag.SelectedCategory = category;
-
             IEnumerable<string> categories = repository.Products
                 .Select(x => x.Category)
                 .Distinct()
                 .OrderBy(x => x);
-
-            return PartialView(categories);
+            return PartialView("FlexMenu", categories);
         }
+        
     }
 }
